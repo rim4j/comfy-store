6971eadfbe12a15abe35a9ae5d036a89d42b9b18
rim4j <amirhosseinjalali818@gmail.com>
2024-09-30 08:38:18 +0330

    setup daisyUi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+import typography from "@tailwindcss/typography";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui, typography],
 };
