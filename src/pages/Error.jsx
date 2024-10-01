9c391726e652ebebf11f3ed196dff0eeda803a38
rim4j <amirhosseinjalali818@gmail.com>
2024-10-01 08:19:25 +0330

    create error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,33 @@
+import { useRouteError, Link } from "react-router-dom";
+
 const Error = () => {
-  return <div>Error</div>;
+  const error = useRouteError();
+
+  if (error.status === 404) {
+    return (
+      <main className='grid min-h-[100vh] place-items-center'>
+        <div className='text-center'>
+          <p className='text-9xl font-semibold text-primary'>404</p>
+          <h1 className='mt-4 text-3xl font-bold tracking-tight sm:text-5xl'>
+            page not found
+          </h1>
+          <p className='mt-6 text-xl leading-7'>
+            sorry we could not find the page your looking for
+          </p>
+          <div className='mt-10'>
+            <Link to='/' className='btn btn-secondary'>
+              go back home
+            </Link>
+          </div>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main className='grid min-h-[100vh] place-items-center'>
+      <h4 className='text-center font-bold text-4xl'> There was an error</h4>
+    </main>
+  );
 };
 
 export default Error;
