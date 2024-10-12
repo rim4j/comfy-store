ca1d2e40a2fd8f1a1b3e5891d91379739a90ebd7
rim4j <amirhosseinjalali818@gmail.com>
2024-10-12 09:46:04 +0330

    single product color

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,5 +1,6 @@
 import { Link, useLoaderData } from "react-router-dom";
 import { customFetch, formatPrice } from "../utils";
+import { useState } from "react";
 
 export const loader = async ({ params }) => {
   const resp = await customFetch(`/products/${params.id}`);
@@ -12,6 +13,8 @@ const SingleProduct = () => {
   const { image, title, price, description, colors, company } =
     product.attributes;
   const dollarsAmount = formatPrice(price);
+
+  const [productColor, setProductColor] = useState(colors[0]);
 
   return (
     <section>
@@ -39,6 +42,27 @@ const SingleProduct = () => {
           </h4>
           <p className='mt-3 text-xl '>{dollarsAmount}</p>
           <p className='mt-6 leading-8 '>{description}</p>
+          {/* colors */}
+          <div className='mt-6'>
+            <h4 className='text-md font-medium tracking-wider capitalize '>
+              colors
+            </h4>
+            <div className='mt-2'>
+              {colors.map((color, i) => {
+                return (
+                  <button
+                    key={i}
+                    type='button'
+                    className={`badge w-6 h-6 mr-2 ${
+                      color === productColor && "border-2 border-secondary"
+                    }`}
+                    style={{ backgroundColor: color }}
+                    onClick={() => setProductColor(color)}
+                  ></button>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </section>
