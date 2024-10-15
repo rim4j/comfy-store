dd80c3cdc48c39f97f6e487ecbe1101efc343070
rim4j <amirhosseinjalali818@gmail.com>
2024-10-15 09:14:55 +0330

    create select filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,7 +1,10 @@
 import { Form, useLoaderData, Link } from "react-router-dom";
 import FormInput from "./FormInput";
+import FormSelect from "./FormSelect";
 
 const Filters = () => {
+  const { meta } = useLoaderData();
+
   return (
     <Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
       {/* search */}
@@ -10,6 +13,27 @@ const Filters = () => {
         label='search products'
         name='search'
         size='sm'
+      />
+      {/* categories */}
+      <FormSelect
+        label='select category'
+        name='category'
+        list={meta.categories}
+        size='select-sm'
+      />
+      {/* companies */}
+      <FormSelect
+        label='select company'
+        name='company'
+        list={meta.companies}
+        size='select-sm'
+      />
+      {/* order */}
+      <FormSelect
+        label='sort by'
+        name='order'
+        list={["a-z", "z-a", "high", "low"]}
+        size='select-sm'
       />
       {/* buttons */}
       <button type='submit' className='btn btn-primary btn-sm'>
