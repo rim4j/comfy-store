dfba632022dc01e3c84fb8a5f44f7c187b72932e
rim4j <amirhosseinjalali818@gmail.com>
2024-10-20 09:19:06 +0330

    redux toolkit setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import { store } from "./store.js";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <>
-    <App />
+    <Provider store={store}>
+      <App />
+      <ToastContainer position='top-center' />
+    </Provider>
   </>
 );
