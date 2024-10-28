cceed83d8631224ac96f28b4a54b99dfc904124a
rim4j <amirhosseinjalali818@gmail.com>
2024-10-28 10:16:42 +0330

    register user

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import { loader as singleProductLoader } from "./pages/SingleProduct";
 import { loader as productsLoader } from "./pages/Products";
 
 //actions
+import { action as registerAction } from "./pages/Register";
 
 const router = createBrowserRouter([
   {
@@ -52,7 +53,12 @@ const router = createBrowserRouter([
     ],
   },
   { path: "/login", element: <Login />, errorElement: <Error /> },
-  { path: "/register", element: <Register />, errorElement: <Error /> },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <Error />,
+    action: registerAction,
+  },
 ]);
 
 const App = () => {
