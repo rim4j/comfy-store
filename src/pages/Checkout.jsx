c0af3221c5f8565374e12f572b0d7319a96ba3ed
rim4j <amirhosseinjalali818@gmail.com>
2024-11-03 09:29:21 +0330

    checkout page setup

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,22 @@
+import { useSelector } from "react-redux";
+import { CartTotals, CheckoutForm, SectionTitle } from "../components";
+
 const Checkout = () => {
-  return <div>Checkout</div>;
+  const { cartTotal } = useSelector((state) => state.cartState);
+
+  if (cartTotal === 0) {
+    return <SectionTitle text='Your cart is empty' />;
+  }
+
+  return (
+    <>
+      <SectionTitle text='place your order'></SectionTitle>
+      <div className='mt-8 grid gap-8 md:grid-cols-2 items-start'>
+        <CheckoutForm />
+        <CartTotals />
+      </div>
+    </>
+  );
 };
 
 export default Checkout;
