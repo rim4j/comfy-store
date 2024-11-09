11ee51960544e7ca86c8b666b95bf5c40767f2cf
rim4j <amirhosseinjalali818@gmail.com>
2024-11-09 10:22:41 +0330

    orders completed

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -42,7 +42,8 @@ export const action =
         error?.response?.data?.error?.message ||
         "there was an error placing your order";
       toast.error(errorMessage);
-      if (error.response.status === 401) return redirect("/login");
+      if (error.response.status === 401 || error.response.status === 403)
+        return redirect("/login");
       return null;
     }
   };
---
+++ src/components/OrdersList.jsx
@@ -0,0 +1,54 @@
+import { useLoaderData } from "react-router-dom";
+import day from "dayjs";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+day.extend(advancedFormat);
+
+const OrdersList = () => {
+  const { meta, orders } = useLoaderData();
+
+  return (
+    <div className='mt-8'>
+      <h4 className='mb-4 capitalize'>
+        total orders : {meta.pagination.total}
+      </h4>
+      <div className='overflow-x-auto'>
+        <table className='table table-zebra'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Products</th>
+              <th>Cost</th>
+              <th className='max-lg:hidden'>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((item) => {
+              const id = item.id;
+              const {
+                address,
+                name,
+                cartItems,
+                createdAt,
+                numItemsInCart,
+                orderTotal,
+              } = item.attributes;
+              const date = day(createdAt).format("hh:mm a - MMM Do, YYYY");
+              return (
+                <tr key={id}>
+                  <td>{name}</td>
+                  <td>{address}</td>
+                  <td>{numItemsInCart}</td>
+                  <td>{orderTotal}</td>
+                  <td className='max-lg:hidden'>{date}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default OrdersList;
